a68b47c47bad75c4dab65f5edd7e4689f883ee75
Ricardo MG Silva <up201607780@fe.up.pt>
2018-02-19 13:31:28 +0000

    Samples code

---
+++ instructionsSet.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+	 printf("hello world!\n");
+	printf("gcc -o hello hello.c\n");
+	printf("to run the program: $ ./hello\n");
+	 exit(0);
+}
