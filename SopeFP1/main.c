9ae8ad3789e3a3ac93ad634083c3e05556261bab
Ricardo MG Silva <up201607780@fe.up.pt>
2018-03-19 11:08:03 +0000

    SopeFP1 preparation

---
+++ SopeFP1/main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: notus
+ *
+ * Created on 12 de Março de 2018, 16:15
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
